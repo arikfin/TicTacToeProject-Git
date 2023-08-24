a2110ccb2f916647eb080aa46bfda3c063e55e16
arikfin <arikfinkel94@gmail.com>
2023-08-24 22:27:49 +0300

    v0.2
    a few changes in AnimatedBackground.js

--- TicTacToe/components/AnimatedBackground.js
+++ TicTacToe/components/AnimatedBackground.js
@@ -1,99 +1,84 @@
 import React, { useEffect, useRef } from 'react';
-import { View, Animated, StyleSheet,Dimensions ,Easing  } from 'react-native';
+import { View, Animated, StyleSheet, Dimensions } from 'react-native';
 import { Svg, Text } from 'react-native-svg';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
-const NUM_ROWS = 5;
-const NUM_COLUMNS = 5;
-const CELL_WIDTH = SCREEN_WIDTH / NUM_COLUMNS;
-const CELL_HEIGHT = SCREEN_HEIGHT / (NUM_ROWS + 1); // +1 to account for the entire screen height
-
-
+const NUM_ELEMENTS = 20; // Number of falling X's and O's
 
 export default function AnimatedBackground() {
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-
-
     const animatedValues = [];
 
     // Initialize animated values for each X or O
-    for (let i = 0; i < NUM_ROWS * NUM_COLUMNS; i++) {
+    for (let i = 0; i < NUM_ELEMENTS; i++) {
         animatedValues.push({
-            translateY: new Animated.Value(0),
-            translateX: new Animated.Value(0)
+            translateY: new Animated.Value(-50), // Start above the screen
+            rotate: new Animated.Value(0),
         });
     }
 
-
     const renderShapes = () => {
         let shapes = [];
-        for(let row = 0; row < NUM_ROWS; row++) {
-            for(let col = 0; col < NUM_COLUMNS; col++) {
-                const xOffset = col * CELL_WIDTH - CELL_WIDTH; // Start off-screen to the left
-                const yOffset = row * CELL_HEIGHT;
-                const index = row * NUM_COLUMNS + col;
-                const isX = Math.random() > 0.5;
-                // Original set
-                shapes.push(renderShape(isX, xOffset, yOffset, `${row}-${col}-original`, animatedValues[index].translateX));
-                // Duplicated set (off-screen to the left)
-                shapes.push(renderShape(isX, xOffset - SCREEN_WIDTH, yOffset, `${row}-${col}-duplicate`, animatedValues[index].translateX));
-            }
+        for (let i = 0; i < NUM_ELEMENTS; i++) {
+            const isX = Math.random() > 0.5;
+            const xOffset = Math.random() * SCREEN_WIDTH;
+            const translateY = animatedValues[i].translateY;
+            const rotate = animatedValues[i].rotate;
+            shapes.push(
+                renderShape(isX, xOffset, i, translateY, rotate)
+            );
         }
         return shapes;
     };
-
-    const renderShape = (isX, xOffset, yOffset, key, translateX) => {
+    
+    const renderShape = (isX, xOffset, key, translateY, rotate) => {
         return (
             <Animated.View key={key} style={{
                     ...styles.svgElement,
-                    top: yOffset,
                     left: xOffset,
-                    transform:[
-                        {translateX: translateX}
-                    ] 
-                }}
-                    >
-    
-                <Svg height="50" width="50">
-                    <Text
-                        fill='white'
-                        stroke='none'
-                        fontSize='24'
-                        x='25'
-                        y='25'
-                        textAnchor='middle'
-                        dy='.3em'
-                    >
-                        {isX ? 'X' : 'O'}
-                    </Text>
-                </Svg>
+                    transform: [
+                        { translateY: translateY },
+                        { rotate: rotate.interpolate({
+                            inputRange: [0, 360],
+                            outputRange: ['0deg', '360deg'],
+                          }) }
+                    ]
+                }}>
+                <Icon name={isX ? "times" : "circle-o"} size={50} color="#E8F0FF" />
             </Animated.View>
         );
     };
 
-
     useEffect(() => {
-        const animations = animatedValues.map((value) => {
+        const fallingAnimations = animatedValues.map((value) => {
             return Animated.loop(
-                Animated.timing(value.translateX, {
-                    toValue: SCREEN_WIDTH,
-                    duration: 5000,
-                    easing: Easing.linear, // Constant speed
+                Animated.timing(value.translateY, {
+                    toValue: SCREEN_HEIGHT + 50, // End below the screen
+                    duration: Math.random() * 5000 + 5000,
                     useNativeDriver: true
                 })
             );
         });
-    
-        Animated.parallel(animations).start();
-    }, []);
-    
 
+        const rotationAnimations = animatedValues.map((value) => {
+            return Animated.loop(
+                Animated.timing(value.rotate, {
+                    toValue: 360,
+                    duration: 2000,
+                    useNativeDriver: true
+                })
+            );
+        });
+
+        Animated.stagger(500, fallingAnimations).start();
+        Animated.parallel(rotationAnimations).start(); // Start rotation animations in parallel
+
+    }, []);
 
     return (
         <View style={styles.container}>
             {renderShapes()}
-            <Animated.View style={{ ...styles.overlay, opacity: fadeAnim }} />
         </View>
     );
 }
@@ -108,19 +93,10 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: '#66D7D1', 
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#66D7D1',
     },
     svgElement: {
         position: 'absolute',
         zIndex: 1,
     },
-    overlay: {
-        ...StyleSheet.absoluteFillObject,
-        zIndex: 0,
-        backgroundColor: 'blue',
-    },
 });
-
-
