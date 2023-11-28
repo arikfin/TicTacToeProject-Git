0c3d36a4d33b9f25078fb82477d312d3bc10da29
arikfin <arikfinkel94@gmail.com>
2023-11-28 18:47:33 +0200

    v0.5.9.4
    need to add indicators and clickable animations

--- TicTacToe/components/Board.js
+++ TicTacToe/components/Board.js
@@ -1,119 +1,243 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
-import Cell from './Cell';
-import Animated, { Easing, useSharedValue, useAnimatedStyle, withTiming } from "react-native-reanimated";
-
+import Cell from "./Cell";
+import Animated, {
+  Easing,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 export default function Board({ onGameEnd, onPlayerChange }) {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [currentPlayer, setCurrentPlayer] = useState('X');
-    const [gameOver, setGameOver] = useState(false);
-    const verticalLineHeight = useSharedValue(0);
-    const horizontalLineWidth = useSharedValue(0);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [gameOver, setGameOver] = useState(false);
+  const verticalLineHeight = useSharedValue(0);
+  const horizontalLineWidth = useSharedValue(0);
+  const [winningCombination, setWinningCombination] = useState(null);
 
-    useEffect(() => {
-        verticalLineHeight.value = withTiming(300, { duration: 1000, easing: Easing.linear });
-        horizontalLineWidth.value = withTiming(300, { duration: 1000, easing: Easing.linear });
-        onPlayerChange(currentPlayer);
-    }, [currentPlayer]);
-
-    const verticalAnimatedStyle = useAnimatedStyle(() => {
-        return {
-            height: verticalLineHeight.value,
-            top: 300 - verticalLineHeight.value,
-        };
+  useEffect(() => {
+    verticalLineHeight.value = withTiming(300, {
+      duration: 1000,
+      easing: Easing.linear,
     });
-
-    const horizontalAnimatedStyle = useAnimatedStyle(() => {
-        return {
-            width: horizontalLineWidth.value,
-            left: 0,
-        };
+    horizontalLineWidth.value = withTiming(300, {
+      duration: 1000,
+      easing: Easing.linear,
     });
+    onPlayerChange(currentPlayer);
+  }, [currentPlayer]);
 
-    const GridLines = () => {
-        return (
-            <>
-                <Animated.View style={[styles.line, { width: 2, height: 300, left: 100 }, verticalAnimatedStyle]} />
-                <Animated.View style={[styles.line, { width: 2, height: 300, left: 200 }, verticalAnimatedStyle]} />
-                <Animated.View style={[styles.line, { width: 300, height: 2, top: 100 }, horizontalAnimatedStyle]} />
-                <Animated.View style={[styles.line, { width: 300, height: 2, top: 200 }, horizontalAnimatedStyle]} />
-            </>
-        );
+  const verticalAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      height: verticalLineHeight.value,
+      top: 300 - verticalLineHeight.value,
+    };
+  });
+
+  const horizontalAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      width: horizontalLineWidth.value,
+      left: 0,
+    };
+  });
+
+  const GridLines = () => {
+    return (
+      <>
+        <Animated.View
+          style={[
+            styles.line,
+            { width: 2, height: 300, left: 100 },
+            verticalAnimatedStyle,
+          ]}
+        />
+        <Animated.View
+          style={[
+            styles.line,
+            { width: 2, height: 300, left: 200 },
+            verticalAnimatedStyle,
+          ]}
+        />
+        <Animated.View
+          style={[
+            styles.line,
+            { width: 300, height: 2, top: 100 },
+            horizontalAnimatedStyle,
+          ]}
+        />
+        <Animated.View
+          style={[
+            styles.line,
+            { width: 300, height: 2, top: 200 },
+            horizontalAnimatedStyle,
+          ]}
+        />
+      </>
+    );
+  };
+
+  const handlePress = (index) => {
+    if (board[index] || gameOver) return;
+
+    const newBoard = board.slice();
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+
+    const winnerCombination = checkWinner(newBoard, currentPlayer);
+    if (winnerCombination) {
+      setGameOver(true);
+      setWinningCombination(winnerCombination);
+      onGameEnd(currentPlayer);
+      return;
     }
 
+    if (checkWinner(newBoard, currentPlayer)) {
+      setGameOver(true);
+      onGameEnd(currentPlayer);
+      return;
+    } else if (newBoard.every((cell) => cell)) {
+      setGameOver(true);
+      onGameEnd("Draw");
+      return;
+    }
 
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
 
+  const checkWinner = (board, player) => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // rows
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // columns
+      [0, 4, 8],
+      [2, 4, 6], // diagonals
+    ];
 
+    for (let combination of winningCombinations) {
+      if (combination.every((index) => board[index] === player)) {
+        return combination; // Return the winning combination
+      }
+    }
+    return null; // No winner
+  };
 
-    const handlePress = (index) => {
-        if (board[index] || gameOver) return;
+  const WinningLine = ({ combination }) => {
+    if (!combination) return null;
 
-        const newBoard = board.slice();
-        newBoard[index] = currentPlayer;
-        setBoard(newBoard);
-
-        if (checkWinner(newBoard, currentPlayer)) {
-            setGameOver(true);
-            onGameEnd(currentPlayer);
-            return;
-        } else if (newBoard.every(cell => cell)) {
-            setGameOver(true);
-            onGameEnd('Draw');
-            return;
-        }
-
-        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
-
-
+    let lineStyle = {
+      position: "absolute",
+      backgroundColor: "red",
+      height: 2,
     };
 
-    const checkWinner = (board, player) => {
-        const winningCombinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-            [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-            [0, 4, 8], [2, 4, 6]  // diagonals
-        ];
+    // Horizontal Lines
+    if (
+      combination.includes(0) &&
+      combination.includes(1) &&
+      combination.includes(2)
+    ) {
+      lineStyle = { ...lineStyle, width: 300, top: 50, left: 0 };
+    } else if (
+      combination.includes(3) &&
+      combination.includes(4) &&
+      combination.includes(5)
+    ) {
+      lineStyle = { ...lineStyle, width: 300, top: 150, left: 0 };
+    } else if (
+      combination.includes(6) &&
+      combination.includes(7) &&
+      combination.includes(8)
+    ) {
+      lineStyle = { ...lineStyle, width: 300, top: 250, left: 0 };
+    }
+    // Vertical Lines
+    else if (
+      combination.includes(0) &&
+      combination.includes(3) &&
+      combination.includes(6)
+    ) {
+      lineStyle = { ...lineStyle, width: 2, height: 300, top: 0, left: 50 };
+    } else if (
+      combination.includes(1) &&
+      combination.includes(4) &&
+      combination.includes(7)
+    ) {
+      lineStyle = { ...lineStyle, width: 2, height: 300, top: 0, left: 150 };
+    } else if (
+      combination.includes(2) &&
+      combination.includes(5) &&
+      combination.includes(8)
+    ) {
+      lineStyle = { ...lineStyle, width: 2, height: 300, top: 0, left: 250 };
+    }
+    // Diagonal Lines
+    else if (
+      combination.includes(0) &&
+      combination.includes(4) &&
+      combination.includes(8)
+    ) {
+      lineStyle = {
+        ...lineStyle,
+        width: 2,
+        height: 416,
+        top: -58,
+        left: 149,
+        transform: [{ rotate: "-45deg" }],
+      };
+    } else if (
+      combination.includes(2) &&
+      combination.includes(4) &&
+      combination.includes(6)
+    ) {
+      lineStyle = {
+        ...lineStyle,
+        width: 2,
+        height: 416,
+        top: -58,
+        left: 149,
+        transform: [{ rotate: "45deg" }],
+      };
+    }
 
-        return winningCombinations.some(combinations => {
-            return combinations.every(index => board[index] === player);
-        });
-    };
+    return <View style={lineStyle} />;
+  };
 
-    return (
-        <View style={styles.container}>
-            {board.map((cell, index) => (
-                <Cell key={index} value={cell} onPress={() => handlePress(index)} />
-            ))}
-            <GridLines />
-        </View>
-    );
-
+  return (
+    <View style={styles.container}>
+      {board.map((cell, index) => (
+        <Cell key={index} value={cell} onPress={() => handlePress(index)} />
+      ))}
+      <GridLines />
+      <WinningLine combination={winningCombination} />
+    </View>
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-    container: {
-        width: 300,
-        height: 300,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-        borderRadius: 10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,  // This is for Android
+  container: {
+    width: 300,
+    height: 300,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    line: {
-        position: "absolute",
-        backgroundColor: "black",
-    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, // This is for Android
+  },
+  line: {
+    position: "absolute",
+    backgroundColor: "black",
+  },
 });
--- TicTacToe/screens/GameScreen.js
+++ TicTacToe/screens/GameScreen.js
@@ -1,41 +1,48 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-import Board from '../components/Board.js';
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import Board from "../components/Board.js";
 
 export default function GameScreen({ navigation }) {
   const [gameResult, setGameResult] = useState(null);
   const [boardKey, setBoardKey] = useState(0);
-  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [xWins, setXWins] = useState(0);
+  const [oWins, setOWins] = useState(0);
+  const [draws, setDraws] = useState(0);
 
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
         console.log("User signed out");
-        navigation.navigate('Login');
+        navigation.navigate("Login");
       })
       .catch((error) => console.log("Error signing out:", error));
   };
 
   const handleGameEnd = (result) => {
-    if (result === 'X' || result === 'O') {
-      Alert.alert('Game Over', `${result} wins!`);
-    } else if (result === 'Draw') {
-      Alert.alert('Game Over', 'It\'s a draw!');
+    if (result === "X") {
+      setXWins(xWins + 1);
+      Alert.alert("Game Over", "X wins!");
+    } else if (result === "O") {
+      setOWins(oWins + 1);
+      Alert.alert("Game Over", "O wins!");
+    } else if (result === "Draw") {
+      setDraws(draws + 1);
+      Alert.alert("Game Over", "It's a draw!");
     }
     setGameResult(result);
-  }
+  };
 
   const handleRestart = () => {
     setGameResult(null);
-    setBoardKey(prevKey => prevKey + 1);
-  }
+    setBoardKey((prevKey) => prevKey + 1);
+  };
 
   const handlePlayerChange = (player) => {
     setCurrentPlayer(player);
-  }
+  };
 
   return (
     <View style={styles.container}>
@@ -44,19 +51,42 @@ export default function GameScreen({ navigation }) {
       </TouchableOpacity>
 
       <View style={styles.turnIndicators}>
-        <View style={[styles.turnIndicator, currentPlayer === 'X' ? styles.activeIndicator : {}]}>
+        <View
+          style={[
+            styles.turnIndicator,
+            currentPlayer === "X" ? styles.activeIndicator : {},
+          ]}
+        >
           <Text style={styles.turnText}>X's Turn</Text>
         </View>
-        <View style={[styles.turnIndicator, currentPlayer === 'O' ? styles.activeIndicator : {}]}>
+        <View
+          style={[
+            styles.turnIndicator,
+            currentPlayer === "O" ? styles.activeIndicator : {},
+          ]}
+        >
           <Text style={styles.turnText}>O's Turn</Text>
         </View>
       </View>
 
-      <Board key={boardKey} onGameEnd={handleGameEnd} onPlayerChange={handlePlayerChange} />
+      <Board
+        key={boardKey}
+        onGameEnd={handleGameEnd}
+        onPlayerChange={handlePlayerChange}
+      />
+
+      <View style={styles.scoreBoard}>
+        <Text style={styles.scoreText}>X Wins: {xWins}</Text>
+        <Text style={styles.scoreText}>O Wins: {oWins}</Text>
+        <Text style={styles.scoreText}>Draws: {draws}</Text>
+      </View>
 
       <View style={styles.restartButtonContainer}>
         {gameResult && (
-          <TouchableOpacity style={styles.restartButton} onPress={handleRestart}>
+          <TouchableOpacity
+            style={styles.restartButton}
+            onPress={handleRestart}
+          >
             <Text style={styles.buttonText}>Restart Game</Text>
           </TouchableOpacity>
         )}
@@ -68,45 +98,45 @@ export default function GameScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
-    backgroundColor: '#007BFF'
+    backgroundColor: "#007BFF",
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
-    color: 'white'
+    color: "white",
   },
   signOutButton: {
-    position: 'absolute',
+    position: "absolute",
     top: 80,
     left: 10,
     padding: 10,
-    backgroundColor: '#FF6347', // Tomato color
-    borderRadius: 5
+    backgroundColor: "#FF6347", // Tomato color
+    borderRadius: 5,
   },
   restartButtonContainer: {
     height: 50, // Reserve space for the button
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%', // Ensure it spans the full width
-    bottom: -50
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%", // Ensure it spans the full width
+    bottom: -50,
   },
   restartButton: {
     padding: 10,
     width: 150,
-    backgroundColor: '#32CD32',
+    backgroundColor: "#32CD32",
     borderRadius: 5,
-    alignItems: 'center'
+    alignItems: "center",
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold'
+    color: "white",
+    fontWeight: "bold",
   },
   turnIndicators: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginBottom: 20,
   },
   turnIndicator: {
@@ -114,15 +144,42 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     borderRadius: 10,
     borderWidth: 2,
-    borderColor: '#FFF',
-    backgroundColor: '#007BFF'
+    borderColor: "#FFF",
+    backgroundColor: "#007BFF",
   },
   activeIndicator: {
-    backgroundColor: '#32CD32', // Light green background for the active player
+    backgroundColor: "#32CD32", // Light green background for the active player
   },
   turnText: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 20
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  scoreBoard: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "#FFF", // You can choose a color that fits your design
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5, // For Android
+  },
+  scoreText: {
+    fontSize: 18,
+    color: "#007BFF", // Text color, can be adjusted
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#007BFF", // Border color
+    borderRadius: 5,
+    minWidth: 80, // Ensures that the frame has a minimum width
+    textAlign: "center", // Centers the text
   },
 });
