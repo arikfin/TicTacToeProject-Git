488c3151013e08bd75a37c90e9d04b6db09b0a77
arikfin <arikfinkel94@gmail.com>
2023-11-30 01:01:20 +0200

    v.0.5.9
    need to a cpu for singleplayer

--- TicTacToe/components/Cell.js
+++ TicTacToe/components/Cell.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-export default function Cell({value, onPress}){
-    return(
-        <TouchableOpacity style={styles.cell} onPress={onPress}>
-            <Text style={styles.text}>{value}</Text>
-        </TouchableOpacity>
-    );
+export default function Cell({ value, onPress }) {
+  let iconName = "";
+  let iconColor = "#7DBBC3"; // Adjust color as needed
+
+  if (value === "X") {
+    iconName = "times";
+  } else if (value === "O") {
+    iconName = "circle-o";
+  }
+
+  return (
+    <TouchableOpacity style={styles.cell} onPress={onPress}>
+      {iconName ? <Icon name={iconName} size={60} color={iconColor} /> : null}
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    cell: {
-        width: 100,
-        height: 100,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-    }
+  cell: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
